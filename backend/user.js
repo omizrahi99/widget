95f46dfa1b8dd2eca62b5159f885bcb94b95fb8d
Anthonypella <anthonypella0@gmail.com>
2023-09-23 13:27:15 -0700

    added backend

---
+++ backend/user.js
@@ -0,0 +1,12 @@
+
+class User {
+    constructor(walletAdr, sessionKey, payDate, publicKey, userState) {
+      this.walletAdr = walletAdr;
+      this.sessionKey = sessionKey;
+      this.payDate = payDate;
+      this.publicKey = publicKey;
+      this.userState = userState;
+    }
+  
+  }
+  
