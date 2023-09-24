570b0059cc8535a7d1ad5f9ce775fe1286ce84d6
Ori Mizrahi <orizmizrahi@gmail.com>
2023-09-24 03:59:55 -0400

    create session

--- frontend/src/helpers/ContractAddresses.js
+++ frontend/src/helpers/ContractAddresses.js
@@ -1,3 +1,3 @@
-export default ContractAddress = {
-    'linea': '0x28A7f0977bbc4F698FE19408B33AbBC73D2191e8'
-}
+export const ContractAddresses = {
+  linea: "0x28A7f0977bbc4F698FE19408B33AbBC73D2191e8",
+};
