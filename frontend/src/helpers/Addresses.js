b08af16892c0742609cb68fd5adb49a6aabb41ff
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2023-09-23 22:05:14 -0400

    add helpers

---
+++ frontend/src/helpers/Addresses.js
@@ -0,0 +1,5 @@
+export default Addresses = {
+    "Smart Account Implementation": "0x00006B7e42e01957dA540Dc6a8F7C30c4D816af5",
+    "Smart Account Factory": "0x000000F9eE1842Bb72F6BBDD75E6D3d4e3e9594C",
+    "Token Paymaster": "0x00000f7365cA6C59A2C93719ad53d567ed49c14C"
+}
